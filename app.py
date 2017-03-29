590ac2be379082c1f2c933975c4cd7cd63adf210
SachinRamki <sach27th@gmail.com>
2017-03-29 22:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def processRequest(req):
 def makeWebhookResult():
         speechz = "" 
         # print(json.dumps(item, indent=4))
-        URL2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=pharmacies%20in%20thudialur&key=AIzaSyCYNf8FttdjFy8_SQORxX6ska6Xji4xEe0&format=json"     googleResponse = urllib.urlopen(URL2)
+        URL2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=pharmacies%20in%20thudialur&key=AIzaSyBa1S1nslOslJn0je4OcVJ38YmBYs51KkY"
+        googleResponse = urllib.urlopen(URL2)
         jsonResponse = json.loads(googleResponse.read())
         #pprint.pprint(jsonResponse)
         #test = json.dumps([s['name'] for s in jsonResponse['results']], indent=4)
